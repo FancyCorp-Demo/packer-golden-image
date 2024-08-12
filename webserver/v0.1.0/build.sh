a9d5a3ba049444eabf115f332bc41ff3d1d098bd
Lucy Davinhart <lucy@lucymhdavies.com>
2024-08-12 12:48:05 +0100

    Add HCP Org IDs and Project ID

--- webserver/v0.1.0/build.sh
+++ webserver/v0.1.0/build.sh
@@ -16,6 +16,9 @@ doormat login -v || doormat login && eval $(doormat aws export --account aws_luc
 # Azure
 # Not needed, as we can use the CLI creds
 
+export HCP_ORGANIZATION_ID=ffa120a5-d7b1-4b9c-be17-33a71e45f43f
+export HCP_PROJECT_ID=d6c96d2b-616b-4cb8-b78c-9e17a78c2167
+
 
 
 echo
