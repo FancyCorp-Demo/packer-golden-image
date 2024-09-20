b9c96cb367a2e6e295486f21fa2413f75d85d7d8
Lucy Davinhart <lucy@lucymhdavies.com>
2024-09-20 18:00:49 +0100

    list projects

--- images/tfc-dashboard/html/scripts.js
+++ images/tfc-dashboard/html/scripts.js
@@ -5,9 +5,8 @@ function init() {
     dialog.showModal();
   });
 
-  document.getElementById("list-orgs").addEventListener("click", () => {
-    listOrgs();
-  });
+  document.getElementById("list-orgs").addEventListener("click", () => { listOrgs(); });
+  document.getElementById("list-projects").addEventListener("click", () => { listProjects(); });
 
   document.getElementById("api-token").value = localStorage.getItem("TFE_TOKEN");
 
@@ -25,6 +24,9 @@ function init() {
 function getAPIToken() {
   return document.getElementById("api-token").value
 }
+
+
+orgs = []
 
 function listOrgs() {
   // https://www.freecodecamp.org/news/how-to-send-http-requests-using-javascript/
@@ -45,30 +47,102 @@ function listOrgs() {
       return response.json()
     })
     .then(json => {
-      // TODO: store this internally
+      orgs = []
 
-
-      response = document.getElementById("response")
-      response.innerHTML = JSON.stringify(json, null, 2);
-
-
-      // Clear the visualisation
-      visual = document.getElementById("visual")
-      visual.innerHTML = ""
+      console.log(json)
 
       for (i in json.data) {
         org = json.data[i]
-        console.log(org)
-
-        orgDiv = document.createElement('div');
-        orgDiv.classList.add('org')
-        orgDiv.appendChild(document.createTextNode(org.id))
-        visual.append(orgDiv)
-
+        orgs.push(org)
       }
 
-
+      visualise()
     })
 
     .catch((error) => console.error("Fetch error:", error)); // In case of an error, it will be captured and logged.
+}
+
+projects = {}
+
+function listProjects() {
+  projects = {}
+
+  for (i in orgs) {
+    org = orgs[i]
+    listProjectsInOrg(org.id)
+  }
+}
+
+function listProjectsInOrg(orgName) {
+  fetch("/tfc/organizations/" + orgName + "/projects", {
+    headers: {
+      "Content-Type": "application/vnd.api+json",
+      "Authorization": "Bearer " + getAPIToken(),
+    },
+  })
+    .then((response) => {
+      // If the response is not 2xx, throw an error
+      if (!response.ok) {
+        console.error("Network response was not ok")
+        throw new Error("Network response was not ok");
+      }
+
+      return response.json()
+    })
+    .then(json => {
+
+      projects[orgName] = []
+
+      console.log(orgName)
+      console.log(json)
+
+      for (i in json.data) {
+        project = json.data[i]
+        projects[orgName].push(project)
+      }
+
+      visualise()
+    })
+
+    .catch((error) => console.error("Fetch error:", error)); // In case of an error, it will be captured and logged.
+}
+
+function visualise() {
+  // Clear the visualisation
+  visual = document.getElementById("visual")
+  visual.innerHTML = ""
+
+  for (i in orgs) {
+    org = orgs[i]
+
+    orgDiv = document.createElement('div');
+    visual.append(orgDiv);
+
+    orgDiv.classList.add('org')
+    orgHeading = document.createElement('h1')
+    orgHeading.appendChild(document.createTextNode(org.id))
+    // TODO: make this a link to the org
+    orgDiv.appendChild(orgHeading)
+
+
+
+    for (j in projects[org.id]) {
+      project = projects[org.id][j]
+
+      projectDiv = document.createElement('div');
+      orgDiv.append(projectDiv)
+
+      projectDiv.classList.add('project')
+      projectHeading = document.createElement('h1')
+      projectHeading.appendChild(document.createTextNode(project.attributes.name))
+      // TODO: make this a link to the project
+      projectDiv.appendChild(projectHeading)
+
+
+      // TODO: workspaces
+    }
+
+
+  }
+
 }
